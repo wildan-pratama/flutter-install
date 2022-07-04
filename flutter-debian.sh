90eb8e5b570413ea84d6dd1eefd4cbb037cde7ef
Leviathan-bit <84622086+Leviathan-bit@users.noreply.github.com>
2022-07-04 13:48:51 +0700

    Update flutter-debian.sh

--- flutter-debian.sh
+++ flutter-debian.sh
@@ -4,8 +4,8 @@ sudo apt-get install git clang cmake ninja-buil openjdk-11-jdk openjdk-11-jre
 
 git clone https://github.com/flutter/flutter.git /home/Android/flutter
 sdkmanager "platform-tools"
-sdkmanager "build-tools;33.0.0-rc4"
-sdkmanager "platforms;android-Tiramisu"
+sdkmanager "build-tools;33.0.0"
+sdkmanager "platforms;android-33"
 sdkmanager --licenses
 flutter doctor --android-licenses
 flutter doctor -v
