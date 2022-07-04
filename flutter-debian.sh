7a5c5c2a5870dce0543e9c697808f607e6d2c6f4
Leviathan-bit <84622086+Leviathan-bit@users.noreply.github.com>
2022-07-04 14:59:27 +0700

    Update flutter-debian.sh

--- flutter-debian.sh
+++ flutter-debian.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install git clang cmake ninja-build openjdk-11-jdk openjdk-11-jre libgtk-3-dev
+sudo apt-get install git clang cmake ninja-build openjdk-11-jdk openjdk-11-jre libgtk-3-dev -y
 
 git clone https://github.com/flutter/flutter.git /home/Android/flutter
 sdkmanager "platform-tools"
