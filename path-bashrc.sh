53e7531c3b697b56653ad2343514ec2c5205dbc7
Wildan Pratama <kadalgurun521@gmail.com>
2023-03-30 15:24:45 +0700

    Update path-bashrc.sh

--- path-bashrc.sh
+++ path-bashrc.sh
@@ -3,12 +3,12 @@
 echo '# SDK exporting' >> ~/.bashrc
 echo 'export ANDROID_HOME="/home/Android"' >> ~/.bashrc
 echo 'export ANDROID_SDK_ROOT="$ANDROID_HOME/sdk"' >> ~/.bashrc
-
+echo '' >> ~/.bashrc
 echo '# Tools exporting ' >> ~/.bashrc
 echo 'export PATH="$PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin"' >> ~/.bashrc
 echo 'export PATH="$PATH:$ANDROID_SDK_ROOT/tools/bin"' >> ~/.bashrc
 echo 'export PATH="$PATH:$ANDROID_SDK_ROOT/emulator"' >> ~/.bashrc
 echo 'export PATH="$PATH:$ANDROID_SDK_ROOT/platform-tools"' >> ~/.bashrc
-
+echo '' >> ~/.bashrc
 echo '# Flutter binary exporting' >> ~/.bashrc
 echo 'export PATH="$PATH:$ANDROID_HOME/flutter/bin"' >> ~/.bashrc
